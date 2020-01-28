5f0eecef8a55aeacfa41be03f20b29d36220ee50
Rails Udemy Course <zaquemuaddib@yahoo.com>
2020-01-28 13:30:33 -0800

    edit, update, show, and index listing and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,20 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+	
+
 	def new
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+
+	end
+
 	def create 
-		@article = Article.new(article_parameters)
+		@article = Article.new(article_params)
 		if @article.save
 			# do something
 			flash[:notice] = "Article was successfully created"
@@ -14,13 +24,23 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+				if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
 
 
 	private
-		def article_parameters
+		def article_params
 			params.require(:article).permit(:title, :description)
 		end
 
